2ad1aaf869dfd1751d1d86933f901bfa0b30eb8f
Cyril SCHMITT <cyrilguy.schmitt@gmail.com>
2017-07-10 15:26:29 -0700

    update test-cyril-loop.py still debug to be done but improvements has been made.

--- test-cyril-loop.py
+++ test-cyril-loop.py
@@ -8,7 +8,6 @@ import operator
 
 class OpticalFlow:
 
-	iterator=0
 	def __init__(self, margin, frame_x, frame_y,angle,pos_x,pos_y,scale,result_filepath=None, data_filepath=None):
 		self.margin=margin
 		self.frame_x=frame_x
@@ -17,6 +16,18 @@ class OpticalFlow:
 		self.pos_x=pos_x
 		self.pos_y=pos_y
 		self.scale=scale
+
+		feature_params_orb = dict ( nfeatures=50,
+                            scaleFactor=2,
+                            nlevels=5,
+                            edgeThreshold=20, 
+                            firstLevel=0, 
+                            WTA_K=4, 
+                            patchSize=30
+                            )
+
+		self.orb = cv2.ORB_create(**feature_params_orb)
+
 
 	def formatting(self, key_points):
 		totalTab=[]
@@ -39,103 +50,158 @@ class OpticalFlow:
 		inpu_t,outpu_t=self.MatchingFeatures(des_old, des_current, feats_old, feats_current)
 
 		# Affine Transform
-        R, t=self.AffineTransform(inpu_t, outpu_t)
+		R, t=self.AffineTransform(inpu_t, outpu_t)
 
         # Estimation Angle and Position
-        angle_calc=self.rad2deg(np.arctan2(-R.item([1][0]),R.item([0][0])))
-        self.angle+=angle_calc
-        translation_x=t[0]*np.cos(self.angle)
-        translation_y=t[1]*np.cos(self.angle)
-        self.pos_x+=translation_x
-        self.pos_y+=translation_y
+		angle_calc=self.rad2deg(np.arctan2(-R.item([1][0]),R.item([0][0])))
+		self.angle+=angle_calc
+		translation_x=t[0]*np.cos(self.angle)
+		translation_y=t[1]*np.cos(self.angle)
+		self.pos_x+=translation_x
+		self.pos_y+=translation_y
 
 		return (total_translation_x,total_translation_y), total_angle
 
-    def ExtractingFeaturesORB(self, frame):
-    	working_frame=frame[self.margin*self.frame_x:(1-self.margin)*self.frame_x , self.margin*self.frame_y:(1-self.margin)*self.frame_y]
-        kp, des = orb.detectAndCompute(working_frame,mask=None)
-        feats = self.formatting(kp)
-    	return kp,des,feats
+	def ExtractingFeaturesORB(self, frame):
+		working_frame=frame[int(self.margin*self.frame_x):int((1-self.margin)*self.frame_x) , int(self.margin*self.frame_y):int((1-self.margin)*self.frame_y)]
+		kp, des = self.orb.detectAndCompute(working_frame,mask=None)
+		feats = self.formatting(kp)
+		return kp,des,feats
 
-    def MatchingFeatures(self, des_old, des_current, feats_old, feats_current):
-    	# Feature Matcher Init
-    	bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
-        matches = bf.knnMatch(des_old,des_current, k=2)
-        good = []
-        # KNN Matching using David Lowe Criteria
-        for m,n in matches:
-            if m.distance < 0.45*n.distance:
-                good.append([m])
-        # Ordering Feature Matched
-        inpu_t = np.zeros((np.shape(good)[0],2))
-        outpu_t = np.zeros((np.shape(good)[0],2))
-        for i in range(np.shape(good)[0]):
-            inpu_t[i]=feats_old[good[i][0].queryIdx]
-            outpu_t[i]=feats_current[good[i][0].trainIdx]
-        return inpu_t,outpu_t
+	def MatchingFeatures(self, des_old, des_current, feats_old, feats_current):
+		# Feature Matcher Init
+		bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+		matches = bf.knnMatch(des_old,des_current, k=2)
+		good = []
+		# KNN Matching using David Lowe Criteria
+		for m,n in matches:
+			if m.distance < 0.45*n.distance:
+				good.append([m])
+		# Ordering Feature Matched
+		inpu_t = np.zeros((np.shape(good)[0],2))
+		outpu_t = np.zeros((np.shape(good)[0],2))
+		for i in range(np.shape(good)[0]):
+			inpu_t[i]=feats_old[good[i][0].queryIdx]
+			outpu_t[i]=feats_current[good[i][0].trainIdx]
+		return inpu_t,outpu_t
 
-    def AffineTransform(self, inpu_t, outpu_t):
-    	# Credit goes to Nghiaho, modification for this case made by me
-	    assert len(inpu_t) == len(outpu_t)
-	    N = inpu_t.shape[0]; # total points
-	    centroid_input = np.mean(inpu_t, axis=0)
-	    centroid_output = np.mean(outpu_t, axis=0)
-	    # centre the points to their center of mass
-	    AA = inpu_t - np.tile(centroid_A, (N, 1))
-	    BB = outpu_t - np.tile(centroid_B, (N, 1))
-	    H = np.dot(np.transpose(AA),BB)
-	    U, S, Vt = np.linalg.svd(H)
-	    R = np.dot(Vt.T,U.T)
-	    # Recenter rotation to centre of frame and not to the left top corner
-	    recenter=centroid_input-[(1-2*margin)/2*frame_x,(1-2*margin)/2*frame_y]
-	    recenter=np.dot(R,recenter)+[(1-2*margin)/2*frame_x,(1-2*margin)/2*frame_y]
-	    t= centroid_output-recenter
-	    return R, t
+	def AffineTransform(self, inpu_t, outpu_t):
+		# Credit goes to Nghiaho, modification for this case made by me
+		assert len(inpu_t) == len(outpu_t)
+		N = inpu_t.shape[0]; # total points
+		centroid_input = np.mean(inpu_t, axis=0)
+		centroid_output = np.mean(outpu_t, axis=0)
+		# centre the points to their center of mass
+		AA = inpu_t - np.tile(centroid_input, (N, 1))
+		BB = outpu_t - np.tile(centroid_output, (N, 1))
+		H = np.dot(np.transpose(AA),BB)
+		U, S, Vt = np.linalg.svd(H)
+		R = np.dot(Vt.T,U.T)
+		# Recenter rotation to centre of frame and not to the left top corner
+		recenter=centroid_input-[(1-2*self.margin)/2*self.frame_x,(1-2*self.margin)/2*self.frame_y]
+		recenter=np.dot(R,recenter)+[(1-2*self.margin)/2*self.frame_x,(1-2*self.margin)/2*self.frame_y]
+		t= centroid_output-recenter
+		return R, t
 
-	def RandT_2_angleAndProjection(R,t):
-		angle=rad2deg(np.arctan2(-R.item([1][0]),R.item([0][0]))))
+	def actualize_pos_orientation(self,R,t):
+		angle=self.rad2deg(np.arctan2(-R.item([1][0]),R.item([0][0])))
 		self.angle+=angle
-		self.pos_x=t[0]*cos(self.angle)
-		self.pos_y=t[1]*cos(self.angle)
-		estimated_pos_x_tab.append(projected_trans_x)
-		estimated_pos_y_tab.append(projected_trans_y)
-		estimated_orientation_tab.append(self.angle)
-		return estimated_pos_x_tab,estimated_pos_y_tab,estimated_orientation_tab
+		self.pos_x=t[0]*np.cos(self.angle)
+		self.pos_y=t[1]*np.cos(self.angle)
+		return 
 
 	def create_dataframe(self, pos_x=0, pos_y=0, angle=0):
-	    frame=cv2.imread("Total3.jpg",0)
-	    (rows,cols)=np.shape(frame)
-	    M=cv2.getRotationMatrix2D((pos_x+self.frame_x/2,y+pos_self.frame_y/2), angle, self.scale)
-	    frame=cv2.warpAffine(frame,M,(rows,cols))
-	    mask=frame[y:y+self.frame_y,x:x+self.frame_x]
-	    return mask
+		frame=cv2.imread("Total3.jpg",0)
+		(rows,cols)=np.shape(frame)
+		M=cv2.getRotationMatrix2D((pos_x+self.frame_x/2,pos_y+self.frame_y/2), angle, self.scale)
+		frame=cv2.warpAffine(frame,M,(rows,cols))
+		mask=frame[pos_y:pos_y+self.frame_y,pos_x:pos_x+self.frame_x]
+		return mask
 
 	def run_test(self, pos_x_tab, pos_y_tab ,orientation_tab):
-		for i in range(np.shape(pos_x_tab)[0]):
-			img_0 = self.create_dataframe(pos_x_tab[i], pos_y_tab[i], orientation_tab[i])
-			img_1 = self.create_dataframe(pos_x_tab[i+1], pos_y_tab[i+1], orientation_tab[i+1])
+		estimated_pos_x_tab=[]
+		estimated_pos_y_tab=[]
+		estimated_orientation_tab=[]
+		for i in range(np.shape(pos_x_tab)[0]-1):
+			img_0=self.create_dataframe(pos_x_tab[i], pos_y_tab[i], orientation_tab[i])
+			img_1=self.create_dataframe(pos_x_tab[i+1], pos_y_tab[i+1], orientation_tab[i+1])
 
-			kp_0,des_0,feats_0 = self.ExtractingFeaturesORB(img_0)
-			kp_1,des_1,feats_1 = self.ExtractingFeaturesORB(img_1)
+			kp_0,des_0,feats_0=self.ExtractingFeaturesORB(img_0)
+			kp_1,des_1,feats_1=self.ExtractingFeaturesORB(img_1)
 
-			matched_0,matched_1 = MatchingFeatures(self, des_0, des_1, feats_0, feats_1)
+			matched_0,matched_1=self.MatchingFeatures(des_0, des_1, feats_0, feats_1)
 
-			R,t = self.AffineTransform(matched_0, matched_1)
+			if np.shape(matched_0)[0]==0:
+				self.estimating()
+			else:
+				R,t=self.AffineTransform(matched_0, matched_1)
+				self.actualize_pos_orientation(R,t)
 
-			estimated_pos_x_tab,estimated_pos_y_tab,estimated_orientation_tab = RandT_2_angleAndProjection(R,t)
+			estimated_pos_x_tab.append(self.pos_x)
+			estimated_pos_y_tab.append(self.pos_y)
+			estimated_orientation_tab.append(self.angle)
 
-		plot_visualization(pos_x_tab,pos_y_tab,orientation_tab, estimated_pos_x_tab, estimated_pos_y_tab, estimated_orientation_tab)
+		self.plot_visualization(pos_x_tab,pos_y_tab,orientation_tab, estimated_pos_x_tab, estimated_pos_y_tab, estimated_orientation_tab)
 
 
+	def estimating(self):
+		# DO SOME KALMAN FILTERING TO ESTIMATED VALUE OF UNFOUND MATHCING FEATURES DISPLACEMENT'S
+		self.pos_x+=0
+		self.pos_y+=0
+		self.angle+=0
+		return 
 
 	def plot_visualization(self, pos_x_tab,pos_y_tab,orientation_tab, estimated_pos_x_tab, estimated_pos_y_tab, estimated_orientation_tab):
-		# ADD VISUALIZATION
+		img=cv2.imread("Total3.jpg",1)
+		fontFace=cv2.FONT_HERSHEY_SIMPLEX
+		fontScale=1.5
 
+		color_real=(0,255,0)
+		color_estimation=(255,0,0)
 
+		### PLOT REAL DATA
+		cv2.circle(img,(int(pos_x_tab[0]),int(pos_y_tab[0])),30,color_real,5)
+		#Draw orientation of robot
+		orientation_line=tuple(map(operator.add,(pos_x_tab[0],pos_y_tab[0]),(30*np.cos(orientation_tab[0]),30*np.sin(orientation_tab[0]))))
+		cv2.line(img,(int(pos_x_tab[0]),int(pos_y_tab[0])),(int(orientation_line[0]),int(orientation_line[1])),color_real, 3)
+		for i in range(np.shape(pos_x_tab)[0]-1):
+			cv2.circle(img,(int(pos_x_tab[i+1]),int(pos_y_tab[i+1])),30,color_real,5)
+			# Add text next to each circle plot
+			text_pos=tuple(map(operator.add,(int(pos_x_tab[i+1]),int(pos_y_tab[i+1])),(40,40)))
+			cv2.putText(img, str(i+1), text_pos, fontFace, fontScale, color_real,thickness=3)
+			#Draw line from one point to an other
+			#cv2.line(img, (int(pos_x_tab[i]),int(pos_y_tab[i])), (int(pos_x_tab[i+1]),int(pos_y_tab[i+1])), color_real, 3) 
+			#Draw orientation of robot
+			orientation_line=tuple(map(operator.add,(pos_x_tab[i+1],pos_y_tab[i+1]),(30*np.cos(orientation_tab[i+1]),30*np.sin(orientation_tab[i+1]))))
+
+			cv2.line(img,(int(pos_x_tab[i+1]),int(pos_y_tab[i+1])),(int(orientation_line[0]),int(orientation_line[1])),color_real, 3)
+
+		### PLOT ESTIMATED DATA
+		cv2.circle(img,(int(estimated_pos_x_tab[0]),int(estimated_pos_y_tab[0])),30,color_estimation,5)
+		#Draw orientation of robot
+		orientation_line=tuple(map(operator.add,(estimated_pos_x_tab[0],estimated_pos_y_tab[0]),(30*np.cos(estimated_orientation_tab[0]),30*np.sin(estimated_orientation_tab[0]))))
+		cv2.line(img,(int(estimated_pos_x_tab[0]),int(estimated_pos_y_tab[0])),(int(orientation_line[0]),int(orientation_line[1])),color_estimation, 3)
+		# print "np.shape(estimated_pos_x_tab)[0]-1",np.shape(estimated_pos_x_tab)[0]-1
+		for i in range(np.shape(estimated_pos_x_tab)[0]-1):
+			# print "estimated_pos_x_tab[i+1] = ",estimated_pos_x_tab[i+1]
+			# print "estimated_pos_y_tab[i+1] = ",estimated_pos_y_tab[i+1]
+			# print "estimated_orientation_tab[i+1] = ",estimated_orientation_tab[i+1]
+			cv2.circle(img,(int(estimated_pos_x_tab[i+1]),int(estimated_pos_y_tab[i+1])),30,color_estimation,5)
+			# Add text next to each circle plot
+			text_pos=tuple(map(operator.add,(int(estimated_pos_x_tab[i+1]),int(estimated_pos_y_tab[i+1])),(40,40)))
+			cv2.putText(img, str(i+1), text_pos, fontFace, fontScale, color_estimation,thickness=3)
+			#Draw line from one point to an other
+			#cv2.line(img, (int(estimated_pos_x_tab[i]),int(estimated_pos_y_tab[i])), (int(estimated_pos_x_tab[i+1]),int(estimated_pos_y_tab[i+1])), color_estimation, 3) 
+			#Draw orientation of robot
+			orientation_line=tuple(map(operator.add,(estimated_pos_x_tab[i+1],estimated_pos_y_tab[i+1]),(30*np.cos(estimated_orientation_tab[i+1]),30*np.sin(estimated_orientation_tab[i+1]))))
+			cv2.line(img,(int(estimated_pos_x_tab[i+1]),int(estimated_pos_y_tab[i+1])),(int(orientation_line[0]),int(orientation_line[1])),color_estimation, 3)
+
+		cv2.imshow("img",img)
+		cv2.waitKey(0)
 
 # INIT VARIABLES
-old_frame=cv2.imread("img-2D-rot/image_"+str(0)+".png",0)
-(frame_x,frame_y)=np.shape(img)
+frame_x=600
+frame_y=600
 margin_ROI=0.1
 angle=0
 pos_x=0
@@ -143,20 +209,14 @@ pos_y=0
 scale=1
 
 #Creation object of Class OpticalFlow
-optflow=OpticalFlow(margin_ROI, frame_x, frame_y, angle, pos_x, pos_y,scale) 
+optflow=OpticalFlow(margin_ROI, frame_x, frame_y, angle, pos_x, pos_y, scale) 
 
-iterator=-1
-all_pos_robot=[]
+#Data
+pos_x_tab=np.array([100,200,300,400,500,600,700,800,900,1000])
+pos_y_tab=np.array([100,200,300,400,500,600,700,800,900,1000])
+orientation_tab=np.array([10,20,30,20,10,0,10,20,30,20])
 
-while(1):
-	iterator+=1
-	frame=cv2.imread("img-2D-rot/image_"+str(iterator)+".png",0)
-	if frame is not None:
-		pos_robot, orientation_robot=optflow.update(old_frame,new_frame)
-		all_pos_robot.append(pos_robot)
-		all_orientation_robot.append(orientation_robot)
-		old_frame=new_frame
-	else:
-		break
+#Run Tets
+optflow.run_test(pos_x_tab, pos_y_tab, orientation_tab)
 
-optflow.plot_visualization(all_pos_robot, all_orientation_robot)
+
